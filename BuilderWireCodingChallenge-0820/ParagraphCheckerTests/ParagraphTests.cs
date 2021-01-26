0d0570ecc67f50412d6265e79385f9df0a1e293c
carl <germanjohncarlo@gmail.com>
2021-01-26 16:05:56 +0800

    Commit Unit Test

---
+++ BuilderWireCodingChallenge-0820/ParagraphCheckerTests/ParagraphTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParagraphChecker;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParagraphChecker.Tests
+{
+    [TestClass()]
+    public class ParagraphTests
+    {
+        string paragraphText = string.Empty;
+        string wordsText = string.Empty;
+
+        [TestMethod()]
+        public void ReadArticleTest()
+        {
+            string articlePath = string.Empty;
+
+            try
+            {
+                articlePath = @"C:\Users\CARL\Desktop\BuilderWireCodingChallenge-0820\Input\Article.txt";
+
+                ProcessFile pf = new ProcessFile();
+                string paragraphText = pf.ReadFile(articlePath);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+
+        }
+
+
+        [TestMethod()]
+        public void ReadWordsTest()
+        {
+            string wordPath = string.Empty;
+            try
+            {
+                wordPath = @"C:\Users\CARL\Desktop\BuilderWireCodingChallenge-0820\Input\Words.txt";
+
+                ProcessFile pf = new ProcessFile();
+                wordsText = pf.ReadFile(wordPath);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+
+        }
+
+        [TestMethod()]
+        public void ParseTest()
+        {
+            try
+            {
+                Paragraph paragraph = new Paragraph();
+                string output = paragraph.Parse(paragraphText, wordsText);
+            }
+            catch {
+                Assert.Fail();
+            }
+        }
+    }
+}
---
+++ BuilderWireCodingChallenge-0820/ParagraphCheckerTests/UtilityTests.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParagraphChecker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParagraphChecker.Tests
+{
+    [TestClass()]
+    public class UtilityTests
+    {
+        [TestMethod()]
+        public void IsUpperTest()
+        {
+            try
+            {
+                bool result = Utility.IsUpper("Test");
+                Assert.IsTrue(result, "True");
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+         
+        }
+
+        [TestMethod()]
+        public void IsLowerTest()
+        {
+            try
+            {
+                bool result = Utility.IsUpper("test");
+                Assert.IsFalse(result, "False");
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+        }
+
+        [TestMethod()]
+        public void SplitTextTest()
+        {
+            try
+            {
+                var List = Utility.SplitText("Test Me");
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void SplitTextByCharTest()
+        {
+            try
+            {
+                var List = Utility.SplitTextByChar("Hello\r\nWorld\r\n", "\r\n");
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void RemoveSpecialCharactersTest()
+        {
+            try
+            {
+                var word = Utility.RemoveSpecialCharacters("Hello.");
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void RemoveSpecialCharacters2Test()
+        {
+            try
+            {
+                var word = Utility.RemoveSpecialCharacters("Hello.");
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
